7ed8e167825c1aa05db73fb6d54ff4e43c270fda
System Administrator <root@Ryans-MacBook-Pro.local>
2016-05-15 20:06:57 -0400

    writes concise commands in order to file

--- HelloWorldApi.py
+++ HelloWorldApi.py
@@ -2,12 +2,12 @@ import tornado.ioloop
 import tornado.web
 from datetime import datetime
 import os
+from operator import itemgetter
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         print(big_list)
         self.write("Hello, world")
-
 
 class PostHandler(tornado.web.RequestHandler):
 
@@ -21,11 +21,38 @@ class PostHandler(tornado.web.RequestHandler):
         command = allowed_commands & command
         file_path = str(os.path.dirname(os.path.realpath(__file__)))+"/session.txt"
         log_entry = str(command)+" "+str(timestamp)
+        log_entries.append((command,timestamp))
         with open(file_path,"a") as writer:
             writer.write(log_entry+"\n")
         print(log_entry)
          
-
+class StoreLogEntriesHandler(tornado.web.RequestHandler):
+    def get(self):
+        file_path = str(os.path.dirname(os.path.realpath(__file__)))+"/clean_session.txt"
+        sorted_log_entries = sorted(log_entries,key=itemgetter(1))
+        prev_command = set()
+        allowed_commands = set(['38','37','39','40'])
+        for log_entry in sorted_log_entries:
+            command = log_entry[0]
+            timestamp = log_entry[1]
+            if len(command ^ prev_command) > 0:
+                prev_command = command
+                with open(file_path,"a") as writer:
+                    readable_command = []
+                    for element in list(command):
+                        if element == '37':
+                            readable_command.append("left")
+                        if element == '38':
+                            readable_command.append("up")
+                        if element == '39':
+                            readable_command.append("right")
+                        if element == '40':
+                            readable_command.append("down")
+                    log_entry = str(list(readable_command))+" "+str(timestamp)
+                    writer.write(log_entry+"\n")
+                print(log_entry)
+            #print(log_entry)
+    
 class MultipleKeysHandler(tornado.web.RequestHandler):
 
     def get(self):
@@ -84,10 +111,12 @@ class MultipleKeysHandler(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/abc",MainHandler),
-        (r"/a",MultipleKeysHandler),(r"/post", PostHandler)
+        (r"/a",MultipleKeysHandler),(r"/post", PostHandler),
+        (r"/StoreLogEntries",StoreLogEntriesHandler)
     ])
 
 if __name__ == "__main__":
+    log_entries = []
     app = make_app()
     app.listen(80)
     tornado.ioloop.IOLoop.current().start()
